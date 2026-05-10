dc3d3a7675da732db0e288d180dcd05a4218534b
adnan <mdajahangir2005@gmail.com>
2026-05-10 23:49:01 +0530

    types of output

---
+++ src/typesOfOutput/Output.java
@@ -0,0 +1,9 @@
+package typesOfOutput;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(" println");
+        System.out.print("year ");
+        System.out.printf("20%d ", 26 );
+    }
+}
