b58fe283c2d021880f936388675393f47a42f287
adnan <mdajahangir2005@gmail.com>
2026-05-10 23:49:57 +0530

    types of output

--- src/typesOfOutput/Output.java
+++ src/typesOfOutput/Output.java
@@ -4,6 +4,9 @@ public class Output {
     public static void main(String[] args) {
         System.out.println(" println");
         System.out.print("year ");
+
         System.out.printf("20%d ", 26 );
+
+
     }
 }
