1b41b81afd1c8d3e82e7d7a23a68dc6f291aa381
adnan <mdajahangir2005@gmail.com>
2026-05-12 22:25:30 +0530

    input for string,double

---
+++ src/Input_output/Input.java
@@ -0,0 +1,21 @@
+package Input_output;
+
+import java.util.Scanner;
+
+public class Input
+{
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String name = input.nextLine();
+        double pi=input.nextDouble();
+
+
+        System.out.println(name+pi);
+
+
+
+    }
+}
