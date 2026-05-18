dbb0410b45aee4b5bce1c4458abcd59e79637b6b
adnan <mdajahangir2005@gmail.com>
2026-05-19 00:19:14 +0530

    print whole numbers

---
+++ src/Loops/WholeNumber.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class WholeNumber {
+    public static void main(String[] args) {
+        System.out.println("Whole Numbers till 10:");
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
