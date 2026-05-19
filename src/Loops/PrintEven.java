3b621f90ccba143fe1d4c5bc162a1b969417795a
adnan <mdajahangir2005@gmail.com>
2026-05-20 00:13:14 +0530

    print even numbers

---
+++ src/Loops/PrintEven.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class PrintEven {
+
+
+    public static void main(String[] args) {
+        System.out.println("Even numbers between 1 and 50:");
+        for (int i = 1; i <= 50; i ++) {
+           if(i%2==0){
+               System.out.print(" " + i);
+
+           }
+        }
+    }
+
+
+}
