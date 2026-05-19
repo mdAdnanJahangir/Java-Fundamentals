ee7a23c543b4da0e0e4125ad9ba3912bf931bab0
adnan <mdajahangir2005@gmail.com>
2026-05-20 00:30:43 +0530

    print even numbers optimize

---
+++ src/Loops/PrintOdd.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class PrintOdd {
+    public static void main(String[] args) {
+        System.out.println("Odd numbers between 1 and 50:");
+        for (int i = 1; i <= 50; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
