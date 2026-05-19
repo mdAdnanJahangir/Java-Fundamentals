dc072903bc5abf4ba0e2d43d13da2f7fce24f7bb
adnan <mdajahangir2005@gmail.com>
2026-05-20 00:18:58 +0530

    print even numbers optimize

--- src/Loops/PrintEven.java
+++ src/Loops/PrintEven.java
@@ -4,14 +4,18 @@ public class PrintEven {
 
 
     public static void main(String[] args) {
-        System.out.println("Even numbers between 1 and 50:");
-        for (int i = 1; i <= 50; i ++) {
-           if(i%2==0){
-               System.out.print(" " + i);
+        //System.out.println("Even numbers between 1 and 50:");
+//        for (int i = 1; i <= 50; i ++) {
+//           if(i%2==0){
+//               System.out.print(" " + i);
+//
+//           }
+        // }
+        System.out.print("Even numbers between 1 and 50:");
+        for (int i = 2; i <= 50; i = i + 2) {
+            System.out.print(" " + i);
 
-           }
         }
+
     }
-
-
 }
