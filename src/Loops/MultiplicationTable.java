76ff203eb8894149d37b28c7bbf3ef31ad0bd21d
adnan <mdajahangir2005@gmail.com>
2026-05-21 21:47:08 +0530

    multiplication table

---
+++ src/Loops/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package Loops;
+
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 5; // you can change this value
+        System.out.println("Multiplication Table of " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
