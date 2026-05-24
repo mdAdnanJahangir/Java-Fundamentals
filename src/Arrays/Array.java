076afe483df6ad54cadcd4706fcc4b52d57259a6
adnan <mdajahangir2005@gmail.com>
2026-05-24 23:38:57 +0530

    array literal and array object

---
+++ src/Arrays/Array.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+
+
+        // static memory allocation
+        int [] arr = {1,2,3,4,5};
+
+        // dynamci memory alocation
+        int [] brr  ;
+        brr = new int[4];
+
+        System.out.println(arr.length);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
