892c1dc024a32f1afab0743ef17b87864c3bd0f1
adnan <mdajahangir2005@gmail.com>
2026-05-26 23:17:55 +0530

    initializing and   printing array with enhanced for loop

--- src/Arrays/Array.java
+++ src/Arrays/Array.java
@@ -9,9 +9,22 @@ public class Array {
 
         // dynamci memory alocation
         int [] brr  ;
-        brr = new int[4];
+        brr = new int[2];
+        brr[0]=1;
+        brr[1]=2;
+
 
         System.out.println(arr.length);
+
+
+        for (int i = 0; i <= arr.length-1; i++) {
+            System.out.print(" " +arr[i]);
+        }
+//
+        for(int i : brr){
+            System.out.print(" " +i);
+
+        }
 
 
 
