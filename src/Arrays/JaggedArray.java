a113a833f4d11554a4d6623e4372b0fb9880c628
adnan <mdajahangir2005@gmail.com>
2026-05-26 23:41:56 +0530

    jagged array

---
+++ src/Arrays/JaggedArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class JaggedArray {
+    public static void main(String[] args) {
+        int [][] arr = new int [2][];
+
+
+        arr[0]= new int [4];
+        arr[1]= new int [2];
+
+
+    }
+}
