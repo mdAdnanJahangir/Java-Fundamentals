dbc24d32ef0391934ab35743bf6161b98c4d1cfd
adnan <mdajahangir2005@gmail.com>
2026-05-26 23:29:54 +0530

    multidimentional array declaring,initializing and printing

---
+++ src/MultiDimentionalArray.java
@@ -0,0 +1,17 @@
+public class MultiDimentionalArray {
+    public static void main(String[] args) {
+
+
+        int[][] arr  = new int[2][2];
+        arr[0][0]=1; arr[0][1]=2;
+        arr[1][0]=1; arr[1][1]=2;
+
+
+        for (int i = 0; i <2 ; i++) {
+            for (int j = 0; j <2 ; j++) {
+                System.out.print(" " + arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
