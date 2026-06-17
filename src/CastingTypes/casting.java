ae0a7bb8705e582b82e0b36bae90696b8bd18f0f
adnan <mdajahangir2005@gmail.com>
2026-06-17 23:26:34 +0530

    type casting

---
+++ src/CastingTypes/casting.java
@@ -0,0 +1,25 @@
+package CastingTypes;
+
+
+
+
+
+class TypeCating{
+
+    static double a =4.5;
+   static int a2 = (int)a;
+   static  float c = (float)a;
+
+
+}
+
+
+
+public class casting {
+    public static void main(String[] args) {
+        System.out.println(  TypeCating.a2);
+        System.out.println(  TypeCating.c);
+
+
+    }
+}
