1878735c3313f5e0330010b6a9811dd0a28f3962
adnan <mdajahangir2005@gmail.com>
2026-07-27 22:46:39 +0530

    method overLoading example2

--- src/methods/MethodOverLoading.java
+++ src/methods/MethodOverLoading.java
@@ -23,9 +23,26 @@ public class MethodOverLoading {
         return 2.22222;
     }
 
+    // here down in add function see return type is same but parameter is different
+    // so hence proved overloading depends on parameters not retrun type
+    //example2
+    static int add(int a ,int b){
+        return  a+b;
+    }
+
+    static int add(double a ,double b){
+        return (int)( a+b);
+    }
 
 
     public static void main(String[] args) {
     show ();
+    int ans1 = add(4,4);
+    int ans2 = add(4.2,4.6);
+
+        System.out.println(ans1);
+        System.out.println(ans2);
+
+
     }
 }
