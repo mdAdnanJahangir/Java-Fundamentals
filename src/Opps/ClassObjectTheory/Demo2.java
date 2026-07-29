1234c9e3c7063955daa0762f8bbcfd4c7df79a51
adnan <mdajahangir2005@gmail.com>
2026-07-29 16:22:17 +0530

    pass by value as reference

---
+++ src/Opps/ClassObjectTheory/Demo2.java
@@ -0,0 +1,36 @@
+package Opps.ClassObjectTheory;
+
+public class Demo2 {
+    // in java there is no pass by reference
+    // only pass by value
+    public static void main(String[] args) {
+        Random r= new Random(4,5);
+        System.out.println("value of x is "+r.x +"  value of y is "+r.y);
+        addTen(r);
+        System.out.println("value of x is "+r.x +"  value of y is "+r.y);
+
+
+
+    }
+
+
+    static void addTen(Random r){
+       r.x=r.x+10;
+       r.y=r.y+10;
+    }
+}
+
+
+
+
+ class Random{
+    int x;
+    int y;
+
+    Random(int x,int y){
+        this.x=x;
+        this.y=y;
+
+    }
+
+ }
