b4df0b1ebb6b90e9d4771305e7734ce1db7a537a
adnan <mdajahangir2005@gmail.com>
2026-07-30 00:07:36 +0530

    auto and unboxing

---
+++ src/AutoUnboxing.java
@@ -0,0 +1,16 @@
+public class AutoUnboxing {
+    public static void main(String[] args) {
+        //autoBoxing
+        int x =10;
+       // Integer y=Integer.valueOf(x);// old way
+        Integer y =x;
+        System.out.println(x);
+        System.out.println(y);// unboxing
+
+
+        // unboxing
+
+
+
+    }
+}
