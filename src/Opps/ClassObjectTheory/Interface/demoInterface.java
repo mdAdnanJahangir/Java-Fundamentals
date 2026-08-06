4dbe0ce0fe341c3d14b3de305eaf5f8ac7068606
adnan <mdajahangir2005@gmail.com>
2026-08-07 01:38:26 +0530

    normal interface

--- src/Opps/ClassObjectTheory/Interface/demoInterface.java
+++ src/Opps/ClassObjectTheory/Interface/demoInterface.java
@@ -1,7 +1,7 @@
 package Opps.ClassObjectTheory.Interface;
 
 public interface demoInterface {
-    // more than one method
+    // more than one method not like functional
 
     void show();
     void show2();
