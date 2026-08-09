a386c1f4c4702509e1da3de5503a41a6e924032c
adnan <mdajahangir2005@gmail.com>
2026-08-09 23:34:56 +0530

    arithmetic exception

---
+++ src/Exception/demo.java
@@ -0,0 +1,10 @@
+package Exception;
+
+public class demo {
+    public static void main(String[] args) {
+        // arithmetic exception
+        int i = 0;
+        int j = 18/i;
+        System.out.println(j);
+    }
+}
