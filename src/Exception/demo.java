398ad9a5d426aac38d04778febbc7a4949732259
adnan <mdajahangir2005@gmail.com>
2026-08-09 23:39:54 +0530

    handling exception with try catch

--- src/Exception/demo.java
+++ src/Exception/demo.java
@@ -4,7 +4,22 @@ public class demo {
     public static void main(String[] args) {
         // arithmetic exception
         int i = 0;
-        int j = 18/i;
+        int j =0;
+
+
+        try{
+             j = 18/i;
+        }catch(Exception e ){
+            System.out.println("someting went wrong" + e);
+        }
+
+
+
         System.out.println(j);
+        System.out.println("code executed  ");
+
+
+
+
     }
 }
