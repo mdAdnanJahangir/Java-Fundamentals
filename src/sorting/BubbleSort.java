800707d553f4f5264d463e59c2479185e881d5bc
adnan <mdajahangir2005@gmail.com>
2026-08-25 23:21:04 +0530

    BubbleSort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,42 @@
+package sorting;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // Stop if array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {2,1,22,3,23,6};
+        bubbleSort(arr);
+        for(int i : arr){
+            System.out.print(" " + i);
+        }
+
+
+
+    }
+}
